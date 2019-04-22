de28fb10d113134bf0aca09ec48d1fb18f1427d5
thebigsteph <nzali.stephane86@gmail.com>
2019-04-22 16:46:25 +0100

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-    has_secure_password
-    before_save { self.email = email.downcase }
+    attr_accessor :remember_token, :activation_token
+    before_save :downcase_email
+    before_create :create_activation_digest
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
@@ -9,6 +9,7 @@ class User < ApplicationRecord
                       uniqueness: {case_sensitive: false}
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
+    has_secure_password
 class << self
     # Returns the hash digest of the given string.
     def digest(string)
@@ -30,8 +31,9 @@ end
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
@@ -39,4 +41,27 @@ end
     update_attribute(:remember_digest, nil)
   end
 
+  # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+private
+
+  #converts email to all lower-case
+  def downcase_email
+      self.email = email.downcase
+  end
+
+  # Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
